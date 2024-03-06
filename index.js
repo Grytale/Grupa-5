072a4ec09a5e6d0f960d287234fc8dac58c9eac3
Grytale <grytale@gmail.com>
2024-03-06 17:53:01 +0200

    Tema

---
+++ index.js
@@ -0,0 +1,5 @@
+const dropdownButton = document.querySelector(".menu-icon");
+const dropdownList = document.querySelector("nav ul");
+dropdownButton.addEventListener("click", function () {
+dropdownList.classList.toggle("show-dropdown");
+});
